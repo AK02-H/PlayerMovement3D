5fac8db581fe3eef2bc0300e0257c2ac9d6803c1
Amin Khan <65659714+Niblet-0@users.noreply.github.com>
2020-11-20 15:10:27 +0000

    Add files via upload

---
+++ PlayerMovement3D.cs
@@ -0,0 +1,103 @@
+﻿///RPG Level-Up 'Stat Curve' Growth System
+///By Amin 'A.K' Khan
+///
+///A simple 3D character controller 
+///Includes jumping (with a max jump counter for multiple air jumps), seperate speeds for different movement directions and camera rotation, and clamps for vertical rotation.
+///Speed values and jump buttons can be set from the inspector
+///Requires a camera object to be a child of the player object, and an empty transform child to act as the position for the ground checl
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerControl : MonoBehaviour
+{
+    //Components
+    Camera _cam;
+    Rigidbody _myRb;
+    [SerializeField] Transform _footPos;    //Drag in empty transform attatched to the bottom of the player
+
+    //Values
+    [SerializeField] private float _speed = 450f, _strafeSpeed = 200f, _rotationSpeedX = 600f, _rotationSpeedY = 600f, _jumpForce = 5f;
+    public int _maxJumps = 2;
+    [SerializeField] private float yClampNeg = -45, yClampPos = 45;
+    [SerializeField] private LayerMask _layerGround;
+    [SerializeField] private KeyCode _jumpButton;
+
+    //Variables
+    float _forwardMove, _sideMove, _rotDeltY, _rotDeltX, _rotX, _rotY;
+    bool _grounded;
+    [SerializeField] float _footRadius = 0.8f;
+    private int _jumpCounter = 0;
+
+    private void Start()
+    {
+        _myRb = GetComponent<Rigidbody>();
+        _cam = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    private void Update()
+    {
+
+        //Get movement axis
+        _forwardMove = Input.GetAxis("Vertical");
+        _sideMove = Input.GetAxis("Horizontal");
+        
+        //Get mouse axis
+        _rotDeltX = Input.GetAxis("Mouse X");
+        _rotDeltY = Input.GetAxis("Mouse Y");
+        
+        //Set rotation values
+        _rotX += (_rotDeltX * _rotationSpeedX * Time.deltaTime);
+        _rotY += (_rotDeltY * _rotationSpeedY * Time.deltaTime);
+        _rotY = Mathf.Clamp(_rotY, yClampNeg, yClampPos);
+        
+        //Apply camera rotation
+        transform.localRotation = Quaternion.Euler(new Vector3(0f, _rotX, 0f));
+        _cam.transform.localRotation = Quaternion.Euler(new Vector3(-_rotY, 0f, 0f));
+
+        //Jump
+        if (Input.GetKeyDown(_jumpButton))
+        {
+            if (_jumpCounter < _maxJumps)
+            {
+                _myRb.velocity += new Vector3(0, _jumpForce, 0);
+                _jumpCounter++;
+            }
+            else
+            {
+                if (Physics.CheckSphere(_footPos.position, _footRadius, _layerGround))
+                {
+                    _jumpCounter = 0;
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Apply Movement
+        Vector3 movement = (transform.forward * _forwardMove * _speed * Time.fixedDeltaTime) + (transform.right * _sideMove * _strafeSpeed * Time.fixedDeltaTime);
+        _myRb.velocity = new Vector3(movement.x, _myRb.velocity.y, movement.z);
+    }
+    
+    
+    private void OnGUI()
+    {
+        //Lock Cursor
+        if (GUI.Button(new Rect(0, 0, 100, 50), "Lock Cursor"))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //Draw ground checker
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_footPos.position, _footRadius);
+    }
+
+}
